d066c06011062f2a95266382a3ef4a7b5f10a161
K_SaiSriram <84832741+Sriram5-coder@users.noreply.github.com>
2022-10-05 12:21:39 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     long long int n;     cin>>n;     long long int d = n%10;     if(d==0){         cout<<n/10;     }     else if(n<0){         d = n/10;         cout<<d-1;     }     else{         d = n/10;         cout<<d;     } }

--- Integer_Division.cpp
+++ Integer_Division.cpp
@@ -1,22 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    long long int X;
-    cin>>X;
-    long long int K=X%10;
-    if(K==0)
-    {
-        cout<<X/10;
+    long long int n;
+    cin>>n;
+    long long int d = n%10;
+    if(d==0){
+        cout<<n/10;
     }
-    else if(X<0)
-    {
-         K=X/10;
-         cout<<K-1;
+    else if(n<0){
+        d = n/10;
+        cout<<d-1;
     }
-    else
-    {
-        K=X/10;
-        cout<<K;
+    else{
+        d = n/10;
+        cout<<d;
     }
 }
